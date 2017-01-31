b1e0b1ac00214485bd00be032fe1b98befcfa54d
Josiah Kangas <jkangas77@gmail.com>
2017-01-31 13:27:07 +0700

    Yeaaahhh?

---
+++ JOSIAH!/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+  $("#togwd").click(function(){
+     $("#pwd").toggle("slow");
+  });
+
+  $("#togcw").click(function(){
+     $("#pcw").toggle("slow");
+  });
+
+  $("#togmg").click(function(){
+     $("#pmg").toggle("slow");
+  });
+
+  $("#togpe").click(function(){
+     $("#ppe").toggle("slow");
+  });
+
+  $("#togpc").click(function(){
+     $("#ppc").toggle("slow");
+  });
+
+  $("#togap").click(function(){
+     $("#pap").toggle("slow");
+  });
+
+  $("#togms").click(function(){
+     $("#pms").toggle("slow");
+  });
+
+  $("#togc7").click(function(){
+     $("#pc7").toggle("slow");
+  });
+})
